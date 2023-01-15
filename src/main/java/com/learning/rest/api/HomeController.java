06b2a92065993fc554f9925a6f7f8f98dd4dc4d9
sridhar <sridhar.redreddy@safely.com>
2023-01-14 21:41:11 -0500

    Initial Commit

---
+++ src/main/java/com/learning/rest/api/HomeController.java
@@ -0,0 +1,13 @@
+package com.learning.rest.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome!!! from Spring Boot";
+    }
+}
